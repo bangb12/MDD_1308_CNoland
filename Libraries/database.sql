fa763aca674d21c22f1698f253c67d9d07bc5fa2
Christopher Noland <bangb12@fullsail.edu>
2013-08-07 17:36:21 -0400

    Updated readme and database

--- Libraries/database.sql
+++ Libraries/database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 07, 2013 at 09:53 PM
+-- Generation Time: Aug 07, 2013 at 11:36 PM
 -- Server version: 5.5.29
 -- PHP Version: 5.4.10
 
@@ -13,6 +13,21 @@ SET time_zone = "+00:00";
 --
 -- Database: `MDD`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `projects`
+--
+
+CREATE TABLE `projects` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `projectname` varchar(50) DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  `image_path` char(200) DEFAULT NULL,
+  `image_name` char(200) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 MDD_1308_CNoland
 ================
+
+Database User and Pass
+Username: root
+Pass: root
