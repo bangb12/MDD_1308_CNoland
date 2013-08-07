485d53570b5341f5549db4da122f8da0d2c43a77
Christopher Noland <bangb12@fullsail.edu>
2013-08-07 15:54:14 -0400

    Updated database table name and added rows

--- Libraries/database.sql
+++ Libraries/database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 07, 2013 at 09:38 PM
+-- Generation Time: Aug 07, 2013 at 09:53 PM
 -- Server version: 5.5.29
 -- PHP Version: 5.4.10
 
@@ -17,10 +17,14 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `table1`
+-- Table structure for table `users`
 --
 
-CREATE TABLE `table1` (
+CREATE TABLE `users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(18) DEFAULT NULL,
+  `password` varchar(15) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `firstname` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
