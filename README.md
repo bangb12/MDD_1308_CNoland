37561ab261acfa287d746011fe24cf916d7fb78a
Christopher Noland <bangb12@fullsail.edu>
2013-08-20 12:59:50 -0400

    File and folder management

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ MDD_1308_CNoland
 Database User and Pass
 Username: root
 Pass: root
+================
+
+Ensure directory is in your Mamp/htdocs directory with MAMP server running. The site will not run otherwise.
+If you do not have mamp ensure it is in a working directory with your server config.
---
+++ application/views/listing.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="Libraries/Bootstrap/dist/css/bootstrap.css" rel="stylesheet">
+    <title>Listings - Chris Noland's Space</title>
+</head>
+
+<body>
+    <div id="wrapper">
+        <header>
+            <a href="#"><img src="Assets/images/logo.jpg" alt="Chris Noland's Space" width="242px" height="102px" id="logo"></a>
+        </header>
+        <div id="listingcontent">
+            <div class="project">
+                <a href="#"><h1>Project Name</h1></a>
+                <img src="Assets/images/projectplaceholder.gif" class="hidden-phone"/>
+                <p>This would be a description of the project, telling a little bit about the why, what, and how.</p>
+            </div>
+            <div class="project">
+                <a href="#"><h1>Project Name</h1></a>
+                <img src="Assets/images/projectplaceholder.gif" class="hidden-phone"/>
+                <p>This would be a description of the project, telling a little bit about the why, what, and how.</p>
+            </div>
+            <div class="project">
+                <a href="#"><h1>Project Name</h1></a>
+                <img src="Assets/images/projectplaceholder.gif" class="hidden-phone"/>
+                <p>This would be a description of the project, telling a little bit about the why, what, and how.</p>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
