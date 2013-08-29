ad56fa37dfd83533819c73bfa4564c2aa3f0116f
Christopher Noland <bangb12@fullsail.edu>
2013-08-29 10:03:50 -0400

    Style guide for User Profile

--- application/logs/log-2013-08-29.php
+++ application/logs/log-2013-08-29.php
@@ -5,3 +5,4 @@ ERROR - 2013-08-29 15:04:05 --> Username or Password field did not match anythin
 ERROR - 2013-08-29 15:04:15 --> Username or Password field did not match anything in database
 ERROR - 2013-08-29 15:35:44 --> Unable to insert into Datbase
 ERROR - 2013-08-29 15:38:57 --> Severity: Warning  --> session_destroy(): Trying to destroy uninitialized session /Applications/MAMP/htdocs/MDD_1308_CNoland/application/models/loginmodel.php 29
+ERROR - 2013-08-29 15:47:38 --> Unable to insert into Datbase, Validation failed
